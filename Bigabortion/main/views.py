058c1d1980f3579ac13888e44234bcbb6600bf49
gux <gabbyburgard@yahoo.com>
2023-08-14 20:54:43 -0600

    populating db with states

--- Bigabortion/main/views.py
+++ Bigabortion/main/views.py
@@ -5,8 +5,26 @@ import json
 from .encoder import AbortionDataListEncoder,  AbortionDataDetailEncoder
 from .models import AbortionData
 from .acls import getAbortionData, getAbortionWaiting, getAbortionInsurance, getAbortionClinics
-# # Create your views here.
-from django.shortcuts import render
+
+def populate_database():
+        state_names = [
+            "Alabama", "Alaska", "Arizona", "Arkansas", "California",
+            "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+            "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+            "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
+            "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
+            "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+            "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+            "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+            "Tennessee", "Utah", "Vermont", "Virginia",
+            "Washington", "West Virginia", "Wisconsin", "Wyoming"
+        ]
+        
+        for state_name in state_names:
+            AbortionData.objects.create(state=state_name)
+        
+populate_database()
+
 
 def catch_all_view(request):
     return render(request, 'frontend/build/index.html')
