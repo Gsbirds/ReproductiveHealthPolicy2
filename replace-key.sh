98acce4ea4eb6172706281880f945614bf14c412
Gsbirds <gabbyburgard@yahoo.com>
2025-03-17 02:30:39 -0600

    not needed

--- replace-key.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git filter-branch -f --tree-filter '
-if [ -f Backend/config/settings.py ]; then
-    sed -i "s/django-insecure-\$b-v%6zh\!gr7z=e3vryc%\$j_2@m=qlx0d_h\*t)8z=re9%=_m)m/REMOVED_FOR_SECURITY/g" Backend/config/settings.py
-fi' HEAD
